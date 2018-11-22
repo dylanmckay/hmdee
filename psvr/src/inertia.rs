30613bc4e354938f0d8714136ed2eecd39ec220e
Dylan McKay <me@dylanmckay.io>
2018-11-22 15:09:07 +1300

    Implement dual-Madgwick sensor fusion via slerp
    This implements a new sensor fusion algorithm, proposed by @mick-p1982,
    that combines two independent Madgwick filters into one by a Quaternion
    SLERP.
    
    The algorithm is described here:
    https://github.com/dylanmckay/psvr-protocol/issues/14
    
    One of the filters acts as a rotation stabilizer. The other filter has a
    beta value optimized for anti-drift.
    
    Combining the two leads to better head tracking.

--- psvr/src/inertia.rs
+++ psvr/src/inertia.rs
@@ -20,7 +20,8 @@ const SAMPLE_FREQUENCY: u32 = 120;
 /// How many seconds inbetween samples.
 const SAMPLE_PERIOD: f32 = 1.0 / SAMPLE_FREQUENCY as f32;
 /// The Madgwick beta constant for the PSVR.
-const MADGWICK_BETA: f32 = 0.025;
+const MADGWICK_BETA_ANTI_DRIFT: f32 = 0.125;
+const MADGWICK_BETA_STEADINESS: Scalar = 0.035;
 
 /// Inertia information at a point in time.
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
@@ -34,17 +35,27 @@ pub struct Instant {
 /// An inertia sensor.
 #[derive(Debug)]
 pub struct Sensor {
-    /// The Madgwick AHRS implementation.
-    madgwick: ahrs::Madgwick<Scalar>,
+    integrators: Integrators,
     /// The timer.
     timer: delta::Timer,
+}
+
+#[derive(Debug)]
+struct Integrators {
+    /// A Madgwick filter optimized for its anti-drift qualities.
+    anti_drift: ahrs::Madgwick<f32>,
+    /// A Madgwick filter optimized for its steadying qualities.
+    steadiness: ahrs::Madgwick<Scalar>,
 }
 
 impl Sensor {
     /// Creates a new inertia sensor.
     pub fn new() -> Self {
         Sensor {
-            madgwick: ahrs::Madgwick::new(SAMPLE_PERIOD, MADGWICK_BETA),
+            integrators: Integrators {
+                anti_drift: ahrs::Madgwick::new(SAMPLE_PERIOD, MADGWICK_BETA_ANTI_DRIFT),
+                steadiness: ahrs::Madgwick::new(SAMPLE_PERIOD, MADGWICK_BETA_STEADINESS),
+            },
             timer: delta::Timer::new(),
         }
     }
@@ -55,17 +66,31 @@ impl Sensor {
 
         // Change the sample period of the existing Madgwick object.
         // The library doesn't directly support dynamic periods.
-        self.madgwick = ahrs::Madgwick::new_with_quat(delta as _, MADGWICK_BETA, self.madgwick.quat);
+        self.integrators.anti_drift = ahrs::Madgwick::new_with_quat(delta as _, MADGWICK_BETA_ANTI_DRIFT, self.integrators.anti_drift.quat);
+        self.integrators.steadiness = ahrs::Madgwick::new_with_quat(delta as _, MADGWICK_BETA_ANTI_DRIFT, self.integrators.steadiness.quat);
 
-        self.madgwick.update_imu(
+        self.integrators.anti_drift.update_imu(
             &instant.gyroscope.into(),
             &instant.accelerometer.into(),
-        ).expect("failed to run AHRS algorithm");
+        ).expect("failed to run anti-drift madgiwck filter");
+
+        self.integrators.steadiness.update_imu(
+            &instant.gyroscope.into(),
+            &instant.accelerometer.into(),
+        ).expect("failed to run steadiness madgiwck filter");
     }
 
     /// Gets the current orientation of the PSVR headset.
     pub fn hmd_orientation(&self) -> Quaternion {
-        self.madgwick.quat
+        use na::geometry::UnitQuaternion;
+
+        // FIXME: t parameter should be calculated on-the-fly.
+        // https://github.com/dylanmckay/psvr-protocol/issues/14#issuecomment-435378326
+        let t = 0.5;
+
+        let anti_drift = UnitQuaternion::new_normalize(self.integrators.anti_drift.quat);
+        let steadiness = UnitQuaternion::new_normalize(self.integrators.steadiness.quat);
+        UnitQuaternion::slerp(&anti_drift, &steadiness, t).quaternion().clone()
     }
 }
 
